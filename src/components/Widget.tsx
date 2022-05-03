5d3d580b9071952b280150fe61dfbe55eddb4815
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-05-03 19:59:46 -0300

    Initial commit!

---
+++ src/components/Widget.tsx
@@ -0,0 +1,19 @@
+import { Popover } from "@headlessui/react";
+import { ChatTeardropDots } from "phosphor-react";
+
+export function Widget() {
+  return (
+    <Popover className="absolute right-5 bottom-5">
+      <Popover.Panel>Hello World!</Popover.Panel>
+
+      <Popover.Button className="bg-brand-500 px-3 h-12 rounded-full text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+}
